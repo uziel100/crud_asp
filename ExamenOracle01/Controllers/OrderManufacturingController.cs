ed91a12ef192d6309061d2197ea23d293db6eab1
itns.practicas <fatima.gonzalez@itns.com.mx>
2023-06-13 12:04:57 -0700

    CRUD orders

--- ExamenOracle01/Controllers/OrderManufacturingController.cs
+++ ExamenOracle01/Controllers/OrderManufacturingController.cs
@@ -34,5 +34,95 @@ namespace ExamenOracle01.Controllers
             }
             return View(lst);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit(int Id)
+        {
+            OrderManufactureViewModel model = new OrderManufactureViewModel();
+            using (CrudIlusionEntities db = new CrudIlusionEntities())
+            {
+                var prevRegister = db.OrderManufactures.Find(Id);
+                model.ManufacturingOrder = prevRegister.manufacturingOrder;
+                model.Status = prevRegister.status;
+                model.Client = prevRegister.client;
+                model.CutType = prevRegister.cuTtype;
+                model.IdOrden = prevRegister.idOrden;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            try
+            {
+                using (CrudIlusionEntities db = new CrudIlusionEntities())
+                {
+                    var prevRegister = db.OrderManufactures.Find(Id);
+                    db.OrderManufactures.Remove(prevRegister);
+                    db.SaveChanges();
+                }
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(OrderManufactureViewModel model)
+        {
+            try
+            {
+                using (CrudIlusionEntities db = new CrudIlusionEntities())
+                {
+                    var newRegister = new OrderManufacture();
+                    newRegister.manufacturingOrder = model.ManufacturingOrder;
+                    newRegister.status = model.Status;
+                    newRegister.client = model.Client;
+                    newRegister.cuTtype = model.CutType;
+
+                    db.OrderManufactures.Add(newRegister);
+                    db.SaveChanges();
+
+                    return Content("1");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(OrderManufactureViewModel model)
+        {
+            try
+            {
+                using (CrudIlusionEntities db = new CrudIlusionEntities())
+                {
+                    var prevRegister = db.OrderManufactures.Find(model.IdOrden);
+                    prevRegister.manufacturingOrder = model.ManufacturingOrder;
+                    prevRegister.status = model.Status;
+                    prevRegister.client = model.Client;
+                    prevRegister.cuTtype = model.CutType;
+                    db.Entry(prevRegister).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+
+                    return Content("1");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
     }
 }
---
+++ ExamenOracle01/Models/ViewModel/OrderManufactureViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Web;
+
+namespace ExamenOracle01.Models.ViewModel
+{
+    public class OrderManufactureViewModel
+    {
+        public int IdOrden { get; set; }
+        [DisplayName("Orden de Manufactura")]
+        public string ManufacturingOrder { get; set; }
+        [DisplayName("Status")]
+        public string Status { get; set; }
+        [DisplayName("Cliente")]
+        public string Client { get; set; }
+        [DisplayName("Tipo corte")]
+        public string CutType { get; set; }
+    }
+}
